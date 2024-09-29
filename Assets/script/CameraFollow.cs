f65b8af060565b2d64b20fe59aae36a6744106c1
ZhenXiang6 <morris930818@gmail.com>
2024-09-29 20:25:11 +0800

    Initial commit

---
+++ Assets/script/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // 玩家
+    public float smoothing = 5f;
+    private Vector3 offset;
+
+    // 摄像机边界
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+
+        // 限制摄像机位置
+        float posX = Mathf.Clamp(targetCamPos.x, minPosition.x, maxPosition.x);
+        float posY = Mathf.Clamp(targetCamPos.y, minPosition.y, maxPosition.y);
+
+        Vector3 clampedPosition = new Vector3(posX, posY, targetCamPos.z);
+
+        transform.position = Vector3.Lerp(transform.position, clampedPosition, smoothing * Time.deltaTime);
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(new Vector3(minPosition.x, minPosition.y, 0), new Vector3(minPosition.x, maxPosition.y, 0));
+        Gizmos.DrawLine(new Vector3(minPosition.x, maxPosition.y, 0), new Vector3(maxPosition.x, maxPosition.y, 0));
+        Gizmos.DrawLine(new Vector3(maxPosition.x, maxPosition.y, 0), new Vector3(maxPosition.x, minPosition.y, 0));
+        Gizmos.DrawLine(new Vector3(maxPosition.x, minPosition.y, 0), new Vector3(minPosition.x, minPosition.y, 0));
+    }
+
+}
---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        animator.speed = 0.5f;
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        animator.SetFloat("Speed", Mathf.Abs(move));
+
+        animator.SetBool("isGrounded", isGrounded);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
