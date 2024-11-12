e7ac0a2d763db909c6cb0be5fabc9bc17fbc2f1b
Ptorioo <ptorio.agar@gmail.com>
2024-11-12 16:00:58 +0800

    scene loading fx + dialog fx

--- Assets/script/NPCs/NPC Tutorial/NPC Tutorial Talk.cs
+++ Assets/script/NPCs/NPC Tutorial/NPC Tutorial Talk.cs
@@ -16,6 +16,8 @@ public class NPCTutorialTalk : MonoBehaviour
     public GameObject NPC;
     public PlayerController playerController;
 
+    public AudioSource audioSource;
+
     void Start()
     {
         isTalking = false;
@@ -39,6 +41,7 @@ public class NPCTutorialTalk : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E) && !isTalking)
             {
                 isTalking = true;
+                audioSource.Play(0);
                 StopMove();
                 StartDialogue();
             }
@@ -68,6 +71,7 @@ public class NPCTutorialTalk : MonoBehaviour
         // Handle input for dialogue progression
         if (isTalking && Input.GetKeyDown(KeyCode.E))
         {
+            audioSource.Play(0);
             flowerSys.Next(); // Continue dialogue
         }
     }
