97945d609425d07cc341333665fe2a342f207c2a
RiceTooCold <josephcc05@gmail.com>
2024-11-19 15:01:13 +0800

    消失掉落

--- Assets/script/Blocks/Disappear Block.cs
+++ Assets/script/Blocks/Disappear Block.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class DisappearBlock : MonoBehaviour
 {
+    private Rigidbody2D rb;
     void OnCollisionEnter2D(Collision2D collision)
     {
         // Start the coroutine to handle the delayed disappearance
@@ -11,9 +12,12 @@ public class DisappearBlock : MonoBehaviour
 
     private IEnumerator DisappearAfterDelay(float delay)
     {
+        rb = GetComponent<Rigidbody2D>();
+        
         // Wait for the specified amount of time
         yield return new WaitForSeconds(delay);
-        
+        rb.isKinematic = false;
+        yield return new WaitForSeconds(delay);
         // After the delay, deactivate the block
         gameObject.SetActive(false);
     }
